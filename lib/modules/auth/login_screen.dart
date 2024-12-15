c570c5c430e6d6ef3524d9adb1d1e9183dd0f085
Aman Singh <71834118+elysian12@users.noreply.github.com>
2024-12-15 13:34:47 +0530

    Implement login button and loading indicator in LoginScreen
    - Added AppButton for user login functionality with a loading indicator.
    - Integrated flutter_screenutil for responsive design.
    - Updated the layout of LoginScreen to include padding for better UI experience.

--- lib/modules/auth/login_screen.dart
+++ lib/modules/auth/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:patel_samaj_app/common/widgets/app_button.dart';
+import 'package:patel_samaj_app/common/widgets/app_loader.dart';
+import 'package:patel_samaj_app/data/di/service_locator.dart';
 
 @RoutePage()
 class LoginScreen extends StatefulWidget {
@@ -13,7 +17,17 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+        child: AppButton(
+          label: 'Login',
+          onPressed: () async {
+            getIt<AppLoader>().show();
+            await Future.delayed(const Duration(seconds: 2));
+            getIt<AppLoader>().hide();
+          },
+        ),
+      ),
     );
   }
 }
